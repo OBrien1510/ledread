96cbb6b7fec5fee31eb65ac9394a0f7b60b2141f
Hugh <hug_hob@hotmail.com>
2018-02-27 18:26:31 +0000

    Added in the switch/turn off functionality
    about to begin full test with input file

--- ledread/ledread/ledread.py
+++ ledread/ledread/ledread.py
@@ -2,9 +2,19 @@
 import sys
 import re
 from urllib.request import urlopen
+from ledsol import ledsol
 
-
-def read():
+def firstLine():
+    f = urlopen(sys.argv[2])
+    for line in f:
+        command = re.match(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*", str(line))
+        if command == None:
+            matrix = ledsol.create()
+    
+    return matrix
+            
+            
+def read(n):
     cmd = 0
     coordFromX = 0
     coordToX = 0
@@ -37,10 +47,11 @@ def read():
                 coordToX = command.group().split(" ")[3].split(",")[0]
                 coordFromY = command.group().split(" ")[1].split(",")[1]
                 coordFromX = command.group().split(" ")[1].split(",")[0]
-            print("command: ", cmd,)
-            print("from coords: ",int(coordFromX), ",", int(coordFromY))
-            print("to coords: ",int(coordToX), ",",int(coordToY))
-                #print(command.group()[2].split(",")[1])
+        else:
+            ledsol.create(int(line))
 
+    finalMatrix = ledsol.turnOnorOff(coordFromX,coordFromY,coordToX,coordToY,n, cmd)
+    
+    return finalMatrix
 
 """Main module."""
