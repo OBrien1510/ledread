c158003e75362759a8879a855eaf20cb07b0438b
Hugh <hug_hob@hotmail.com>
2018-02-28 14:51:51 +0000

    once ledsol installs ledread as planned
    led read does not import properly anymore even though it is an installed
    module.
    
    Fixed a typo in setup file that should hopefully rectify issue

--- ledread/setup.py
+++ ledread/setup.py
@@ -44,7 +44,7 @@ setup(
     include_package_data=True,
     keywords='ledread',
     name='ledread',
-    packages=find_packages(include=['ledsread']),
+    packages=find_packages(include=['ledread']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
